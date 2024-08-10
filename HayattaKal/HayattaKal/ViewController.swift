1ca0113826ffd34d78c60d0d370431cca5ab728f
Yunus Emre Berdibek <com.yunusberdibekk@gmail.com>
2024-08-10 20:17:20 +0300

    Add: project created

---
+++ HayattaKal/HayattaKal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HayattaKal
+//
+//  Created by Yunus Emre Berdibek on 10.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
+#Preview {
+    ViewController()
+}
